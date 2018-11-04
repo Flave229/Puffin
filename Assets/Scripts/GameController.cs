5c0699a57d66f97381d6e29f804b16496093c064
David Smith <david@penkridge.co.uk>
2018-11-04 02:36:27 +0000

    Bringing Mike's prototype code in line with the outlined coding guidelines

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,19 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
 
-    // Use this for initialization
-    void Start()
-    {
+	// Use this for initialization
+	void Start()
+	{
 
-    }
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
+	// Update is called once per frame
+	void Update()
+	{
 
-    }
+	}
 }
--- Assets/Scripts/PrototypeGameplay/GameplayUtility/PlayerKillingZone.cs
+++ Assets/Scripts/PrototypeGameplay/GameplayUtility/PlayerKillingZone.cs
@@ -1,40 +1,35 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PlayerKillingZone : MonoBehaviour
 {
+	public void Start()
+	{
 
+	}
 
-    public void Start()
-    {
+	public void Update()
+	{
 
-    }
+	}
 
+	private void OnDrawGizmos()
+	{
+		Gizmos.color = new Color(0.5f, 0.0f, 0.0f, 0.75f);
+		Gizmos.DrawWireCube(transform.position, transform.localScale);
+	}
 
-    public void Update()
-    {
+	private void OnDrawGizmosSelected()
+	{
+		Gizmos.color = new Color(1.0f, 0.0f, 0.0f, 1.0f);
+		Gizmos.DrawCube(transform.position, transform.localScale);
+	}
 
-    }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = new Color( 0.5f, 0.0f, 0.0f, 0.75f );
-        Gizmos.DrawWireCube( transform.position, transform.localScale );
-    }
-
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.color = new Color( 1.0f, 0.0f, 0.0f, 1.0f );
-        Gizmos.DrawCube( transform.position, transform.localScale );
-    }
-
-    private void OnTriggerEnter( Collider other )
-    {
-        if ( other.tag == "Player" )
-        {
-            SceneManager.LoadScene( SceneManager.GetActiveScene().name );
-        }
-    }
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+		}
+	}
 }
