b6bc66de1121160c4212fe7d5ed0b3326183b3d0
David Smith <david@penkridge.co.uk>
2018-11-04 15:48:26 +0000

    Death is more dramatic

--- Assets/Scripts/DavidHackPad/AI/AgentBehaviour.cs
+++ Assets/Scripts/DavidHackPad/AI/AgentBehaviour.cs
@@ -2,6 +2,7 @@
 using Assets.Scripts.DavidHackPad.AI.Movement;
 using Assets.Scripts.DavidHackPad.Energy;
 using UnityEngine;
+using Random = System.Random;
 
 namespace Assets.Scripts.DavidHackPad.AI
 {
@@ -12,11 +13,17 @@ namespace Assets.Scripts.DavidHackPad.AI
 		private float _kiloJouleEnergy;
 		private bool _alive;
 
+		// Scan for food every 3 seconds
+		private float _timeSinceLastFoodScan;
+
+		private Food _targetFood;
+
 		void Start()
 		{
 			_rigidbody = GetComponent<Rigidbody>();
 			_kiloJouleEnergy = 200;
 			_alive = true;
+			_timeSinceLastFoodScan = Mathf.Infinity;
 		}
 
 		void Update()
@@ -27,33 +34,23 @@ namespace Assets.Scripts.DavidHackPad.AI
 			}
 
 			// Making energy decay at about 5 kilojoules per second
-			_kiloJouleEnergy -= Time.deltaTime * 5;
+			_kiloJouleEnergy -= Time.deltaTime * 10;
+			_timeSinceLastFoodScan += Time.deltaTime;
 
 			// Agent wants food
-			Food[] foodObjects = FindObjectsOfType<Food>();
+			if (_timeSinceLastFoodScan > 3 || _targetFood == null)
+			{
+				_timeSinceLastFoodScan = 0;
+				FindFood();
+			}
 
-			if (foodObjects.Length == 0)
+			if (_targetFood == null)
 			{
 				return;
 			}
 
-			Food closestFood = foodObjects[0];
-			float closestDistance = Mathf.Infinity;
-			for(int foodIndex = 1; foodIndex < foodObjects.Length; ++foodIndex)
-			{
-				Food food = foodObjects[foodIndex];
-				Vector3 directionTowardsFood = food.transform.position - this.transform.position;
-				float squareDistanceToTarget = directionTowardsFood.sqrMagnitude;
-
-				if (squareDistanceToTarget < closestDistance)
-				{
-					closestDistance = squareDistanceToTarget;
-					closestFood = food;
-				}
-			}
-
 			Seek seekForce = new Seek();
-			Vector3 forceTowardsFood = seekForce.ApplyForce(transform.position, closestFood.transform.position);
+			Vector3 forceTowardsFood = seekForce.ApplyForce(transform.position, _targetFood.transform.position);
 			forceTowardsFood = new Vector3(forceTowardsFood.x, 0, forceTowardsFood.z) * 3;
 
 			// Temp hack that makes them fall over if they run out of energy
@@ -64,10 +61,42 @@ namespace Assets.Scripts.DavidHackPad.AI
 			}
 			else
 			{
-				_alive = false;
-				_rigidbody.constraints = RigidbodyConstraints.None;
+				KillAgent();
 				_rigidbody.AddForce(forceTowardsFood * 30, ForceMode.Force);
 			}
+		}
+
+		private void FindFood()
+		{
+			Food[] foodObjects = FindObjectsOfType<Food>();
+
+			if (foodObjects.Length == 0)
+			{
+				return;
+			}
+
+			_targetFood = foodObjects[0];
+			float closestDistance = Mathf.Infinity;
+			for (int foodIndex = 1; foodIndex < foodObjects.Length; ++foodIndex)
+			{
+				Food food = foodObjects[foodIndex];
+				Vector3 directionTowardsFood = food.transform.position - this.transform.position;
+				float squareDistanceToTarget = directionTowardsFood.sqrMagnitude;
+
+				if (squareDistanceToTarget < closestDistance)
+				{
+					closestDistance = squareDistanceToTarget;
+					_targetFood = food;
+				}
+			}
+		}
+
+		private void KillAgent()
+		{
+			_rigidbody.constraints = RigidbodyConstraints.None;
+			Random randomGenerator = new Random();
+			_rigidbody.AddForce(randomGenerator.Next(5, 10), 0, randomGenerator.Next(5, 10));
+			_alive = false;
 		}
 
 		public void ConsumeEnergy(EnergyCompontent energy)
@@ -75,6 +104,7 @@ namespace Assets.Scripts.DavidHackPad.AI
 			if (energy.EnergyType == EEnergyType.NOURISHMENT)
 			{
 				_kiloJouleEnergy += energy.KiloJoules;
+				FindFood();
 			}
 		}
 	}
