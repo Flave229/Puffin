e0a1272c503af0c8a48fa7b1ca460d9cc7853062
David Smith <david@penkridge.co.uk>
2018-11-04 02:54:22 +0000

    Update the scene Unity!

--- Assets/Scripts/PrototypeGameplay/KeyboardControlCapture.cs
+++ Assets/Scripts/PrototypeGameplay/KeyboardControlCapture.cs
@@ -1,28 +1,25 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class KeyboardControlCapture : MonoBehaviour
 {
+	private bool _isControllerCaptured = false;
 
-    private bool _isControllerCaptured = false;
-    
-    void Update()
-    {
-        if ( Input.GetMouseButton( 0 ) )
-        {
-            _isControllerCaptured = true;
-            Cursor.lockState = CursorLockMode.Locked;
-        }
-        if ( Input.GetKey( KeyCode.Escape ) )
-        {
-            _isControllerCaptured = false;
-            Cursor.lockState = CursorLockMode.None;
-        }
-    }
+	void Update()
+	{
+		if (Input.GetMouseButton(0))
+		{
+			_isControllerCaptured = true;
+			Cursor.lockState = CursorLockMode.Locked;
+		}
+		if (Input.GetKey(KeyCode.Escape))
+		{
+			_isControllerCaptured = false;
+			Cursor.lockState = CursorLockMode.None;
+		}
+	}
 
-    public bool IsControllerCaptured()
-    {
-        return _isControllerCaptured;
-    }
+	public bool IsControllerCaptured()
+	{
+		return _isControllerCaptured;
+	}
 }
